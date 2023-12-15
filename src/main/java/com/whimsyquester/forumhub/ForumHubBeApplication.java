06cdbfb3a613dcf93824e26e3b0ace0c122b95c2
WhimsyQuester <rongquanhuang01@gmail.com>
2023-12-16 01:32:58 +0800

    initialize

---
+++ src/main/java/com/whimsyquester/forumhub/ForumHubBeApplication.java
@@ -0,0 +1,13 @@
+package com.whimsyquester.forumhub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForumHubBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ForumHubBeApplication.class, args);
+    }
+
+}
